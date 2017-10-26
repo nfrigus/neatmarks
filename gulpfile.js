a92a9962e4aa7b4260534fdfdd3a32467b9deefc
Semen Shestakov <shestakovsemen@gmail.com>
2017-10-26 08:43:33 +0300

    Add icon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const gulp = require('gulp')
 const del = require('del')
+const gulp = require('gulp')
 const {exec} = require('child_process')
+const svg2png = require('svg2png')
+const {readFileSync, writeFile} = require('fs')
+const {promisify} = require('util')
 
 gulp.task('clean', () => {
   return del([
@@ -21,9 +24,18 @@ gulp.task('pack', function (cb) {
     cb(err)
   })
 })
+function conertSvg(src, size) {
+  return svg2png(src, {width: size, height: size})
+    .then(buffer => promisify(writeFile)('dist/icons/' + size + '.png', buffer))
+}
+gulp.task('icon', () => {
+  const src = readFileSync('dist/icons/icon.svg')
+  ;[16, 48, 128].forEach(size => conertSvg(src, size))
+})
 
 gulp.task('default', [
   'clean',
   'copy',
+  'icon',
   'pack',
 ])
