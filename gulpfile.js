30d3d8153874337f30b8505945ba4377d1c76bc6
Semen Shestakov <shestakovsemen@gmail.com>
2017-10-28 01:41:44 +0300

    Add bundling with webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,56 @@
 const del = require('del')
 const gulp = require('gulp')
-const {exec} = require('child_process')
 const svg2png = require('svg2png')
-const {readFileSync, writeFile} = require('fs')
+const webpack = require('webpack-stream')(require('./webpack.config'))
+const {exec} = require('child_process')
 const {promisify} = require('util')
+const {readFileSync, writeFile} = require('fs')
 
-gulp.task('clean', () => {
-  return del([
-    'dist/**/*',
-    '!dist/key.pem',
-  ])
-})
-gulp.task('copy', () => {
-  return gulp.src([
-      'src/**/*',
-    ], {base: 'src'})
-    .pipe(gulp.dest('dist'))
-})
-gulp.task('pack', function (cb) {
-  exec('npm run pack', function (err, stdout, stderr) {
-    console.log(stdout)
-    console.error(stderr)
-    cb(err)
-  })
-})
-function conertSvg(src, size) {
-  return svg2png(src, {width: size, height: size})
-    .then(buffer => promisify(writeFile)('dist/icons/' + size + '.png', buffer))
-}
-gulp.task('icon', () => {
-  const src = readFileSync('dist/icons/icon.svg')
-  ;[16, 48, 128].forEach(size => conertSvg(src, size))
-})
+
+gulp.task('clean', () => del([
+  'dist/**/*',
+  '!dist/key.pem',
+]))
+gulp.task('assets', ['clean'], () => gulp.src([
+    'src/**/*',
+    '!src/js/**/*',
+  ], {base: 'src'})
+  .pipe(gulp.dest('dist')))
+gulp.task('icon', ['clean'], () => convertSvg2Pngs({
+  dest: 'dist/icons/{size}.png',
+  sizes: [16, 48, 128],
+  src: 'src/icons/icon.svg',
+}))
+gulp.task('js', ['clean'], () => gulp.src('src')
+  .pipe(webpack)
+  .pipe(gulp.dest('dist/js/')))
+gulp.task('crx', [
+  'assets',
+  'icon',
+  'js',
+], cb => exec('npm run pack', (err, stdout, stderr) => {
+  console.log(stdout)
+  console.error(stderr)
+  cb(err)
+}))
 
 gulp.task('default', [
+  'assets',
   'clean',
-  'copy',
+  'crx',
   'icon',
-  'pack',
+  'js',
 ])
+
+async function convertSvg2Pngs({
+  src,
+  sizes,
+  dest,
+}) {
+  const write = promisify(writeFile)
+  src = readFileSync(src)
+
+  return Promise.all(sizes.map(size =>
+    svg2png(src, {width: size, height: size})
+      .then(buffer => write(dest.replace('{size}', size), buffer))))
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,51 @@
+const webpack = require('webpack')
+
+
 module.exports = {
   devtool: 'source-map',
 
   entry: {
-    content_script: './src/content_script.js',
+    background: './src/js/background.js',
+    bookmarks: './src/js/bookmarks.js',
+    content_script: './src/js/content_script.js',
+    options: './src/js/options.js',
+    popup: './src/js/popup.js',
   },
 
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/dist/js',
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
   },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      'window.jQuery': 'jquery',
+      jQuery: 'jquery',
+      Popper: ['popper.js', 'default'],
+      Vue: ['vue/dist/vue.esm.js', 'default'],
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendors',
+      minChunks: isVendorModule,
+    }),
+  ],
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [
-          'babel-loader',
-        ],
+        use: ['babel-loader'],
         exclude: /node_modules/,
       },
     ],
   },
+}
 
-};
+
+function isVendorModule({context}) {
+  return typeof context === 'string'
+    ? context.indexOf('node_modules') !== -1
+    : false
+}
