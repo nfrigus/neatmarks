f25cbe48c2e3a147eb9fb18a63b5b592646d9e37
Semen Shestakov <shestakovsemen@gmail.com>
2019-11-04 02:52:47 +0200

    test: test bookmarks ordering

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -9,6 +9,69 @@ describe('browser', function () {
     return await page.assertNoConsoleErrors()
   })
 
+  describe('sorting', () => {
+    const links = [{
+      url: "http://b",
+      title: "Link C",
+      index: 0,
+    }, {
+      title: "Folder",
+      index: 1,
+    }, {
+      url: "http://c",
+      title: "Link A",
+      index: 2,
+    }, {
+      url: "http://a",
+      title: "Link B",
+      index: 3,
+    }]
+
+    before(async () => {
+      await browser.navigate('options')
+      const page = await browser.getPage()
+      await page.evaluate(new Function(`chrome.bookmarks.createTree(1, ${JSON.stringify(links)}).catch(console.error)`))
+    })
+
+    it('sorting disabled by default', async () => assertOptions(['none']))
+
+    ;[
+      ['title asc', 'alpha', ["Folder", "Link A", "Link B", "Link C"]],
+      ['title desc', 'alphaReverse', ["Folder", "Link C", "Link B", "Link A"]],
+      ['url asc', 'url', ["Folder", "Link B", "Link C", "Link A"]],
+      ['url desc', 'urlReverse', ["Folder", "Link C", "Link B", "Link A"]],
+    ].forEach(([name, orderBy, assert]) => it(`sort by ${name}`, async () => {
+      await setOptions({
+        orderBy,
+        orderDelay: 0,
+      })
+
+      const bm = await getBookmarks()
+
+      bm.map(i => i.title).should.eqls(assert)
+    }))
+
+
+    async function setOptions(option) {
+      const page = await browser.getPage()
+
+      await page.evaluate(option => window.chrome.extension.sendMessage({ request: 'options.set', option }), option)
+      await page.reload()
+    }
+    async function getBookmarks() {
+      const page = await browser.getPage()
+
+      await page.evaluate(() => chrome.bookmarks.getSubTree("1", ret => { window.ret = ret }))
+
+      return page.evaluate(() => window.ret[0].children)
+    }
+    async function assertOptions(values) {
+      const page = await browser.getPage()
+      const defaultValues = await page.$$eval('[name^="options."]', nodes => nodes.map(node => node.value))
+      defaultValues.should.eqls(values)
+    }
+  })
+
   describe('keyboard tabs navigation', () => {
     before(() => browser.navigate('options'))
 
