6e193cf918453220c61c9ecb249443b8a0f28dc3
Semen Shestakov <shestakovsemen@gmail.com>
2020-04-26 20:17:08 +0300

    feat: review permissions

--- src/app/commands/open_tabs_window.js
+++ src/app/commands/open_tabs_window.js
@@ -2,22 +2,18 @@ const browser = window.chrome;
 
 
 export default function () {
-  browser.storage.sync.get(null, (items) => {
-    const width = typeof items.windowWidth === 'number' ? items.windowWidth : 400;
-
-    browser.windows.getCurrent((win) => {
-      const popupWindow = window.open(
-        browser.extension.getURL('app.html#/tabs'),
-        'Tab manager',
-        [
-          'alwaysOnTop=yes',
-          `height=${win.height - 10}`,
-          `left=${window.screenLeft}`,
-          `top=${window.screenTop - 70}`,
-          `width=${width}`,
-        ].join(','),
-      );
-      popupWindow.initialWindowId = win.id;
-    });
+  browser.windows.getCurrent((win) => {
+    const popupWindow = window.open(
+      browser.extension.getURL('app.html#/tabs'),
+      'Tab manager',
+      [
+        'alwaysOnTop=yes',
+        `height=${win.height - 10}`,
+        `left=${window.screenLeft}`,
+        `top=${window.screenTop - 70}`,
+        'width=400',
+      ].join(','),
+    );
+    popupWindow.initialWindowId = win.id;
   });
 }
--- src/manifest/security.js
+++ src/manifest/security.js
@@ -2,25 +2,14 @@ module.exports = {
   content_security_policy: "script-src 'self' object-src 'self'",
   incognito: "spanning",
   permissions: [
-    "background",
-    "bookmarks",
-    "storage",
-    "tabs",
+    "background", // Allow background tasks: sort, sync, backup, etc
+    "bookmarks",  // Access bookmarks management
+    "tabs",       // Access tabs and windows
   ],
   optional_permissions: [
-    "activeTab",
-    "alarms",
-    "contextMenus",
-    "history",
-    "nativeMessaging",
-    "notifications",
-    "platformKeys",
-    "unlimitedStorage",
+    // "alarms",        // todo: Scheduling backups
+    // "history",       // todo: Manage history
+    // "notifications", // todo: Notifications
+    // "storage",       // todo: sync across browsers
   ],
-  chrome_ui_overrides: {
-    bookmarks_ui: {
-      remove_button: "true",
-      remove_bookmark_shortcut: "true",
-    },
-  },
 }
