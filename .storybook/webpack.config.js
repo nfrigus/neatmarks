a30db86af10b26c6b9374446f9845f998c79bfb5
Semen Shestakov <shestakovsemen@gmail.com>
2020-04-26 19:26:52 +0300

    dev: update dependencies

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,11 @@
 const custom = require('../webpack.config.js')
 const { uniqBy } = require('lodash')
 
-module.exports = async ({ config }) => {
-  const newVar = {
-    ...config,
-    plugins: uniqBy([...custom.plugins, ...config.plugins], i => i.constructor.name),
-    module: {
-      ...config.module,
-      rules: [...custom.module.rules, /*...config.module.rules*/],
-    }
-  }
-
-  return newVar
-}
+module.exports = async ({ config }) => ({
+  ...config,
+  plugins: uniqBy([...custom.plugins, ...config.plugins], i => i.constructor.name),
+  module: {
+    ...config.module,
+    rules: [...custom.module.rules /*...config.module.rules*/],
+  },
+})
