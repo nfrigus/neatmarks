e90c16eb6a110d11b68fae2118ed7b049ac4ca57
Semen Shestakov <shestakovsemen@gmail.com>
2021-01-02 22:45:14 +0200

    dev: Allow Icon to be used from TS components

--- src/app/ui/components/Icon.vue
+++ src/app/ui/components/Icon.vue
@@ -1,8 +1,17 @@
-<script>
+<template>
+  <i :class="getClassNames()" />
+</template>
+<script lang="ts">
   export default {
     name: 'Icon',
-    render() {
-      return <i class={{ fa: 1, fas: 1, [`fa-${this.$slots.default[0].text}`]: 1 }}></i>
+    methods: {
+      getClassNames() {
+        return {
+          [`fa-${this.$slots.default[0].text.trim()}`]: 1,
+          fa: 1,
+          fas: 1,
+        }
+      },
     },
   }
 </script>
