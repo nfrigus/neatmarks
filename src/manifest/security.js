6df023ec1593ffbe08fb644aa861a1cc20cd5fab
Semen Shestakov <shestakovsemen@gmail.com>
2021-01-03 17:09:05 +0200

    fix: Invalid CSP definition in manifest

--- src/manifest/security.js
+++ src/manifest/security.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content_security_policy: "script-src 'self' object-src 'self'",
+  content_security_policy: "script-src 'self'; object-src 'self';",
   incognito: "spanning",
   permissions: [
     "alarms",     // Backups scheduling
