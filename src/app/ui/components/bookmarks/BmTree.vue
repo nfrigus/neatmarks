c238deef6e2c000cd4fb685b4c44f0956ad2c690
Semen Shestakov <shestakovsemen@gmail.com>
2023-02-05 13:50:11 +0200

    feat[ui]: Improve bookmarks tree actions contrast

--- src/app/ui/components/bookmarks/BmTree.vue
+++ src/app/ui/components/bookmarks/BmTree.vue
@@ -40,7 +40,24 @@
 </script>
 
 <style lang="scss">
+  .BmTree {
+    background: #fff;
+  }
+
   .BmTree-Actions {
+    background: RGBA(0, 0, 255, .1);
     font-size: 2em;
+
+    a {
+      display: inline-block;
+      height: 36px;
+      line-height: 36px;
+      text-align: center;
+      width: 36px;
+    }
+
+    a:hover {
+      background: RGBA(0, 0, 0, .1);
+    }
   }
 </style>
